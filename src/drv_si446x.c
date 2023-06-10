3549d2bbcf234fee705d19066260c2a360e2cc45
FlyyingPiggy2020 <154562451@qq.com>
2023-06-10 14:02:06 +0800

    简单实现

--- src/drv_si446x.c
+++ src/drv_si446x.c
@@ -1,6 +1,6 @@
 #include <board.h>
 #include "drv_si446x.h"
-
+#include "radio.h"
 
 /**
  * @brief receive rx buff
@@ -9,7 +9,7 @@
  */
 uint8_t drv_si446x_rx(uint8_t *rxbuf)
 {
-    return 0;
+    return si4460_RF_RX(rxbuf);
 }
 
 /**
@@ -19,11 +19,12 @@ uint8_t drv_si446x_rx(uint8_t *rxbuf)
  */
 void drv_si446x_tx(uint8_t *txbuf, uint8_t length)
 {
-    return;
+    si4460_RF_TX(txbuf,length);
 }
 
 int rt_hw_si446x_init(vodi)
 {
+    si4460_InitRF(868.26);
     return RT_EOK;
 }
 INIT_BOARD_EXPORT(rt_hw_si446x_init);
